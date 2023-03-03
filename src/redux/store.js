02ab57835abdfa05c1a4b59b1c73de63749d0172
Anh Vu Phuc - PTSP <anh.vp@KSS.COM.VN>
2023-03-03 17:03:29 +0700

    them redux thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,12 @@
-
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
 import rootReducer from '../reducer/rootReducer';
-
+import thunkMiddleWare from 'redux-thunk'
 
 const store = createStore(
-
     rootReducer,
-
-    composeWithDevTools(),
-
-);
+    composeWithDevTools(applyMiddleware(thunkMiddleWare)
+    ));
 
 
 export default store;
